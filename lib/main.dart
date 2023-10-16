8d25b56478553275ab5053c8f783e0c53a82c2e1
Shubham Prajapati <shubhampr86@gmail.com>
2023-10-16 22:00:24 +0530

    sort the code

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() {
     theme: ThemeData(
       primaryColor: Colors.indigo,
       brightness: Brightness.dark,
-
     ),
   ));
 }
@@ -31,19 +30,28 @@ class _Iform extends State<Iform> {
     TextStyle? textStyle = Theme.of(context).textTheme.titleMedium;
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text('Interest Calculator'),
       ),
       body: Container(
           margin: EdgeInsets.all(_minimumPadding * 2),
           child: ListView(
-
             children: <Widget>[
-              getImageAssets(),
+              // getImageAssets(),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('images/money.png',width: 125,height: 125,),
+              ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: TextField(
                   style: textStyle,
                   decoration: InputDecoration(
+                    enabledBorder:OutlineInputBorder(
+                      borderSide: BorderSide(width: 3, color: Colors.red),
+                    ),focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(width: 3, color: Colors.greenAccent),
+                  ),
                       label: const Text('Principal'),
                       hintText: "Enter Principal e.g 12000",
                       labelStyle: textStyle,
@@ -85,7 +93,10 @@ class _Iform extends State<Iform> {
                     ),
                     Expanded(
                       child: DropdownButton<String>(
-                        hint: Text('Select currenci', style: textStyle,),
+                        hint: Text(
+                          'Select currencies',
+                          style: textStyle,
+                        ),
                         items: _currencies.map((String value) {
                           return DropdownMenuItem<String>(
                             value: value,
@@ -106,7 +117,6 @@ class _Iform extends State<Iform> {
                   ],
                 ),
               ),
-              
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Row(
@@ -114,45 +124,62 @@ class _Iform extends State<Iform> {
                     Expanded(
                       child: ElevatedButton(
                         style: ElevatedButton.styleFrom(
-                          primary: Theme.of(context).toggleableActiveColor,
-                          backgroundColor: Colors.grey,
-                          textStyle: const TextStyle(
-                            color: Colors.black
-                          )
+                            primary: Theme.of(context).toggleableActiveColor,
+                            backgroundColor: Colors.grey,
+                            textStyle: const TextStyle(color: Colors.black)),
+                        child: const Text(
+                          'Calculate',
+                          textScaleFactor: 1.5,
                         ),
-                        child: const Text('Calculate',textScaleFactor: 1.5,),
                         onPressed: () {},
                       ),
+                    ),TextButton(
+                        onPressed: (){},
+                        child: Text('SJS')
                     ),
-                    Container(width: _minimumPadding *5 ,),
+                    Container(
+                      width: _minimumPadding * 5,
+                    ),
                     Expanded(
                       child: ElevatedButton(
                         onPressed: () {},
-                        child: Text('Reset',style: textStyle,textScaleFactor: 1.5,),
+                        child: Text(
+                          'Reset',
+                          style: textStyle,
+                          textScaleFactor: 1.5,
+                        ),
                       ),
                     ),
                   ],
                 ),
               ),
-              Padding(padding: EdgeInsets.all(_minimumPadding*2,),
-              child: Text('Todo Test',style: textStyle,),)
+              Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.all(
+                  _minimumPadding * 2,
+                ),
+                child: Text(
+                  'Todo Test',
+                  style: textStyle,
+                ),
+              )
             ],
           )),
     );
   }
 
 //fun for images
-  Widget getImageAssets() {
-    AssetImage assetImage = AssetImage('images/money.png');
-    Image image = Image(
-      image: assetImage,
-      width: 125.0,
-      height: 125.0,
-    );
+//   Widget getImageAssets() {
+//     AssetImage assetImage = AssetImage('images/money.png');
+//     Image image = Image(
+//       image: assetImage,
+//       width: 125.0,
+//       height: 125.0,
+//     );
+//     return Container(
+//       child: image,
+//       margin: EdgeInsets.all(_minimumPadding * 10),
+//     );
+//   }
 
-    return Container(
-      child: image,
-      margin: EdgeInsets.all(_minimumPadding * 10),
-    );
-  }
 }
